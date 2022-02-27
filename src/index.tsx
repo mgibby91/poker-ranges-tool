78e90042edf69e292debb077b07558dd964ee1ba
Matthew Gibson <mattgibson6@gmail.com>
2022-02-27 16:38:21 -0700

    feat: init basic redux store and provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Provider as ReduxProvider } from 'react-redux';
+import { store } from './state/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ReduxProvider store={store}>
+      <App />
+    </ReduxProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
