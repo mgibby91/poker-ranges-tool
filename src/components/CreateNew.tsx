12a121954610e3066537e769bbf8244419430f42
Matthew Gibson <mattgibson6@gmail.com>
2022-02-28 20:37:29 -0700

    feat: init modal

--- src/components/CreateNew.tsx
+++ src/components/CreateNew.tsx
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled';
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 type CreateNewProps = {
   onClick: () => void;
@@ -8,20 +10,29 @@ type CreateNewProps = {
 const CreateNew: React.VFC<CreateNewProps> = ({ onClick }) => {
   return (
     <CreateNewWrapper onClick={onClick}>
-      <div>Create new</div>
+      <PlusIcon icon={faPlus} />
     </CreateNewWrapper>
   );
 };
 
 const CreateNewWrapper = styled.div`
-  border: 2px solid #3a3a3c;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid #d7dadc;
   width: 15rem;
   height: 10rem;
   border-radius: 0.5rem;
 
   &:hover {
     cursor: pointer;
+    background-color: #3a3a3c;
   }
+`;
+
+const PlusIcon = styled(FontAwesomeIcon)`
+  font-size: 3rem;
+  color: #d7dadc;
 `;
 
 export default CreateNew;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,15 +1,47 @@
 import styled from '@emotion/styled';
-import React from 'react';
+import React, { useState } from 'react';
 import CreateNew from './CreateNew';
+import Modal from 'react-modal';
+
+const modalStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    width: '80%',
+    height: '80%',
+    maxWidth: '80%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: '#121213',
+    color: '#d7dadc',
+    border: 'none',
+    borderRadius: '0.5rem',
+  },
+  overlay: {
+    backgroundColor: '#ffffff3e',
+  },
+};
 
 const Main = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const onCreateNew = () => {
+    setIsModalOpen(true);
     console.log('create new');
   };
 
   return (
     <MainWrapper>
       <CreateNew onClick={onCreateNew} />
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={() => setIsModalOpen(false)}
+        style={modalStyles}
+      >
+        <div>Modal content</div>
+      </Modal>
     </MainWrapper>
   );
 };
@@ -19,4 +51,5 @@ const MainWrapper = styled.div`
   display: flex;
   margin: 2rem 0;
 `;
+
 export default Main;
